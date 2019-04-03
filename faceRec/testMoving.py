74141fc72416a923644af3793e60ecdfa2cd2a06
bradyjibanez <bradyjibanez@gmail.com>
2019-04-03 17:53:53 +0100

    Further

--- faceRec/testMoving.py
+++
@@ -1,4 +0,0 @@
-from shutil import move
-
-move("/home/brady/Code/AIProject/unknown/Tom-Hardy-net-film-is-about-Bosnian-War-and-heroin-addiction-834610.jpg","/home/brady/Code/AIProject/Tom-Hardy-net-film-is-about-Bosnian-War-and-heroin-addiction-834610.jpg")
-print("copied")
